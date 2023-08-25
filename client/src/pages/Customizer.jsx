e5b5ce72f2031bd50254670d55f1879c4b2b341e
drhcode <doriancocka@gmail.com>
2023-08-25 16:58:56 +0200

    add some new language

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -139,7 +139,7 @@ const Customizer = () => {
           <motion.div className='absolute z-10 top-5 right-5' {...fadeAnimation}>
             <CustomButton
               type='filled'
-              title='Go Back'
+              title='Kthehu mbrapa'
               handleClick={() => (state.intro = true)}
               customStyles='w-fit px-4 py-2.5 font-bold text-sm'
             />
